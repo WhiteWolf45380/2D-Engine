23e470831bba48c972cf99f200a05b917eb15667
WhiteWolf <matheodeponte45@gmail.com>
2026-03-18 11:45:54 +0100

    feat: new component Animator

--- pyverse2d/asset/_animation_request.py
+++ pyverse2d/asset/_animation_request.py
@@ -16,6 +16,7 @@ class AnimationRequest(Asset):
         animation(Animation): animation à activer
         loop(bool, optional): répétition de l'animation
         priority(int, optional): niveau de priorité de l'animation
+        tag(str, optional): label de l'animation
         condition(callable, optional): condition d'activation de l'animation
         on_start(callable, optional): fonction de début d'animation
         on_end(callable, optional): fonction de fin d'animation
@@ -23,6 +24,7 @@ class AnimationRequest(Asset):
     animation: Animation
     loop: bool = False
     priority: int = 0
+    tag: str | None = None
     condition: Callable[[], bool] | None = None
     on_start: Callable[[], None] | None = None
     on_end: Callable[[], None] | None = None
--- pyverse2d/world/_component/_animator.py
+++ pyverse2d/world/_component/_animator.py
@@ -1,8 +1,9 @@
 # ======================================== IMPORTS ========================================
 from __future__ import annotations
 
+from ..._internal import expect
 from ...abc import Component
-from ...asset import Animation
+from ...asset import Animation, AnimationRequest
 
 # ======================================== COMPONENT ========================================
 class Animator(Component):
@@ -12,10 +13,70 @@ class Animator(Component):
     Args:
         idle(Animation, None): animation par défaut
     """
-    __slots__ = ()
+    __slots__ = ("_idle", "_current", "_frame", "_elapsed",  "_requests")
 
     def __init__(self, idle: Animation = None):
-        self._current = idle
-        self._frame = 0
-        self._elapsed = 0.0
+        self._idle: Animation = idle
         self._requests: list[AnimationRequest] = []
+        self._current: Animation | None = idle
+        self._frame: int = 0
+        self._elapsed: float = 0.0
+    
+    # ======================================== CONVERSIONS ========================================
+    def __repr__(self) -> str:
+        """Renvoie une représentation de l'animateur"""
+        return f"Animator(current={self._current}, frame={self._frame})"
+
+    # ======================================== GETTERS ========================================
+    @property
+    def idle(self) -> Animation:
+        """Renvoie l'animation par défaut"""
+        return self._idle
+
+    @property
+    def current(self) -> Animation:
+        """Renvoie l'animation actuelle"""
+        return self._current
+    
+    @property
+    def current_index(self) -> int:
+        """Renvoie l'indice courant"""
+        return self._frame
+
+    @property
+    def current_frame(self) -> Animation:
+        """Renvoie la frame actuelle"""
+        return self._current[self._frame]
+    
+    @property
+    def requests(self) -> list[AnimationRequest]:
+        """Renvoie la liste courante des requêtes"""
+        return self._requests
+
+    # ======================================== PUBLIC ========================================
+    def register(self, request: AnimationRequest) -> None:
+        """
+        Enregistre une requête d'animation
+
+        Args:
+            request(AnimationRequest): requête d'animation à enregistrer
+        """
+        self._requests.append(expect(request, AnimationRequest))
+
+    def unregister(self, request: AnimationRequest) -> None:
+        """
+        Retire une requête d'animation
+
+        Args:
+            request(AnimationRequest): requête d'animation à retirer
+        """
+        self._requests.remove(request)
+    
+    def unregister_tag(self, tag: str) -> None:
+        """
+        Retire les requêtes d'animation avec un certain label
+
+        Args:
+            tag(str): label des requêtes à supprimer
+        """
+        self._requests = [request for request in self._requests if request.tag != tag]
